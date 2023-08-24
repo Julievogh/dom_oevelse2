720e3ac469f6d0e03ac91ae8f453a31485f2efb3
Julie Vogh <juvo0001@stud.kea.dk>
2023-08-24 10:43:11 +0200

    2b

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 let overskrift = document.querySelector("h1");
 console.log(overskrift);
+
+overskrift.textContent = "Velkommen til Medina";
+
 let underoverskrift = document.querySelector("h2");
 console.log(underoverskrift);
-let tekst = document.querySelector("p");
-console.log(tekst);
+
+underoverskrift.textContent = "Her er to spændende artikler om ingenting";
+
+let h3Titel1 = document.querySelector("article:first-child h3");
+
+h3Titel1.textContent = "Pippi er sej";
+
+let h3Titel2 = document.querySelector("article:nth-child(2) h3");
+
+h3Titel2.textContent = "Bla bla bla";
+
+let artikel1 = document.querySelector("article:first-child p");
+console.log(artikel1);
+
+artikel1.textContent = "Her står noget klogt, som er meget imponerende";
+
+let artikel2 = document.querySelector("article:nth-child(2) p");
+console.log(artikel2);
+
+artikel2.textContent = "Her står også noget klogt, wauw!";
+
 let billede = document.querySelector("img");
 console.log(billede);
+
 let article = document.querySelector("article");
 console.log(article);
