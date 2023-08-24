2cf72677c11995e20c5c2853ce883c13ddff89bd
Julie Vogh <juvo0001@stud.kea.dk>
2023-08-24 15:55:54 +0200

    øvelse6

--- script.js
+++ script.js
@@ -1,60 +1,73 @@
-let overskrift = document.querySelector("h1");
-console.log(overskrift);
+/* ØVELSE 5 */
 
-overskrift.textContent = "Velkommen til Medina";
+document.addEventListener("DOMContentLoaded", start);
 
-let underoverskrift = document.querySelector("h2");
-console.log(underoverskrift);
+function start() {
+  let overskrift = document.querySelector("h1");
+  console.log(overskrift);
 
-underoverskrift.textContent = "Her er to spændende artikler om ingenting";
+  overskrift.textContent = "Velkommen til Medina";
 
-let h3Titel1 = document.querySelector("article:first-child h3");
+  let underoverskrift = document.querySelector("h2");
+  console.log(underoverskrift);
 
-h3Titel1.textContent = "Pippi er sej";
+  underoverskrift.textContent = "Her er to spændende artikler om ingenting";
 
-let h3Titel2 = document.querySelector("article:nth-child(2) h3");
+  let h3Titel1 = document.querySelector("article:first-child h3");
 
-h3Titel2.textContent = "Bla bla bla";
+  h3Titel1.textContent = "Pippi er sej";
 
-let artikel1 = document.querySelector("article:first-child p");
-console.log(artikel1);
+  let h3Titel2 = document.querySelector("article:nth-child(2) h3");
 
-artikel1.textContent = "Her står noget klogt, som er meget imponerende";
+  h3Titel2.textContent = "Bla bla bla";
 
-let artikel2 = document.querySelector("article:nth-child(2) p");
-console.log(artikel2);
+  let artikel1 = document.querySelector("article:first-child p");
+  console.log(artikel1);
 
-artikel2.textContent = "Her står også noget klogt, wauw!";
+  artikel1.textContent = "Her står noget klogt, som er meget imponerende";
 
-/* OPGAVE 3 */
+  let artikel2 = document.querySelector("article:nth-child(2) p");
+  console.log(artikel2);
 
-let billede1 = document.querySelector("article:first-child img");
-billede1.setAttribute("src", "https://picsum.photos/id/80/400/300");
-billede1.setAttribute("alt", "grankogler");
+  artikel2.textContent = "Her står også noget klogt, wauw!";
 
-let billede2 = document.querySelector("article:nth-child(2) img");
-billede2.setAttribute("src", "https://picsum.photos/id/90/400/300");
-billede2.setAttribute("alt", "glas på pinde");
+  /* OPGAVE 3 */
 
-/* OPGAVE 4 */
+  let billede1 = document.querySelector("article:first-child img");
+  billede1.setAttribute("src", "https://picsum.photos/id/80/400/300");
+  billede1.setAttribute("alt", "grankogler");
 
-const newArticle = document.createElement("article");
-const newImage = document.createElement("img");
-newImage.src = "https://picsum.photos/id/57/400/300";
-newImage.alt = "Street in the city";
+  let billede2 = document.querySelector("article:nth-child(2) img");
+  billede2.setAttribute("src", "https://picsum.photos/id/90/400/300");
+  billede2.setAttribute("alt", "glas på pinde");
 
-const newHeading = document.createElement("h3");
-newHeading.textContent = "En flot gade i byen";
+  /* OPGAVE 4 */
 
-const newParagraph = document.createElement("p");
-newParagraph.textContent = "Her er der åbenbart ingen, der gider at gå.";
+  const newArticle = document.createElement("article");
+  const newImage = document.createElement("img");
+  newImage.src = "https://picsum.photos/id/57/400/300";
+  newImage.alt = "Street in the city";
 
-const mainElement = document.querySelector("main");
+  const newHeading = document.createElement("h3");
+  newHeading.textContent = "En flot gade i byen";
 
-// Append the new elements to the new article
-newArticle.appendChild(newImage);
-newArticle.appendChild(newHeading);
-newArticle.appendChild(newParagraph);
+  const newParagraph = document.createElement("p");
+  newParagraph.textContent = "Her er der åbenbart ingen, der gider at gå.";
 
-// Insert the new article after the existing articles
-mainElement.appendChild(newArticle);
+  const mainElement = document.querySelector("main");
+
+  // Append the new elements to the new article
+  newArticle.appendChild(newImage);
+  newArticle.appendChild(newHeading);
+  newArticle.appendChild(newParagraph);
+
+  // Insert the new article after the existing articles
+  mainElement.appendChild(newArticle);
+
+  billede1.addEventListener("click", changePicture);
+
+  function changePicture() {
+    billede1.setAttribute("src", "https://picsum.photos/400/300");
+    billede1.setAttribute("alt", "new_image_alt_text_here");
+  }
+}
